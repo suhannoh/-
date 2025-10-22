ce9e07702b82e8fa28cb67bef3478aa581682c9e
suhannoh <suhannoh@gmail.com>
2025-10-22 15:14:27 +0900

    Add files via upload

--- main-Section3.js
+++ main-Section3.js
@@ -2,9 +2,19 @@ const contactBtn = {
     phone : document.querySelector("#contactPhone"),
     chat : document.querySelector("#contactEmail"),
 }
+const $quickBtn = document.querySelector("#quickBtn");
+const q = document.querySelectorAll(".fixed-side-1 li");
 
 contactBtn.phone.addEventListener("click", alertMsg);
 contactBtn.chat.addEventListener("click", alertMsg);
+$quickBtn.addEventListener("click", () => {
+    q.forEach(qq => {
+        qq.classList.toggle("f-hdn");
+    });
+
+
+})
+
 
 function alertMsg() {
     return alert("전화상담과 상담문의 alert 입니다.");
