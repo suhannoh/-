7bd7362d16322442312a68f005643bc51b9195cb
NK <sunder10210@gmail.com>
2025-10-28 11:52:07 +0900

    28마지막1

--- header_Nav.js
+++ header_Nav.js
@@ -1,6 +1,15 @@
 // TesT 
 // git add .
-// git commit -m "1024마지막"
+// git commit -m "28마지막"
 // git checkout "branch_header_Nav"
 // git push origin "branch_header_Nav"
 // 
+
+//임시 
+// git stash
+// git pull origin branch_header_Nav
+// git stash pop
+
+// 강제 현재 지금내꺼 없에고
+// git reset --hard
+// git pull origin branch_header_Nav
